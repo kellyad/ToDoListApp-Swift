93aa5f81d7c1310fe8673818d4b6dfae37dc77b8
kelly michael adiputra <kellyadiputra@atlassian.com>
2019-03-14 11:55:11 +0700

    Initial Commit

---
+++ SimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by evhive on 14/03/19.
+//  Copyright © 2019 Coco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
